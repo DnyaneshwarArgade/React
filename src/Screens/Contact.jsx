6715b4b6370d3df0eb4a0fbc3ea893fb7251bf5b
Dnyaneshwar Argade <dnyaneshwarargade8@gmail.com>
2025-08-01 11:32:52 +0530

    Contact Page in Odd Number

--- src/Screens/Contact.jsx
+++ src/Screens/Contact.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-export default function Contact() {
+export default function Contact() { 
+
+  const oddnumber = [];
+
+  for(let i = 1; i <= 50; i++)
+  {
+    if(i % 2 !== 0)
+    {
+      oddnumber.push(i);
+    }
+  }
   return (
     <div>
-      <h1>This is Contact Page...................</h1>
+      <h4>This is Odd Number...................</h4>
+        <ul>
+          {oddnumber.map((num, index) => (
+            <li key={index}> {num} </li>
+          ))}
+        </ul>
     </div>
   )
 }
