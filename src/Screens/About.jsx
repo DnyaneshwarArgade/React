39615889d03a8dc480f373b568090d1bf6da6d9b
Dnyaneshwar Argade <dnyaneshwarargade8@gmail.com>
2025-08-01 11:19:33 +0530

    Even Number Print in React

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 export default function About() {
+
+  const num = [];
+
+  for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+      num.push(i);
+    }
+  }
   return (
-    <div>
-      <h1>This is About Page.................</h1>
+    <div style={{padding : 10}}>
+      <h4>This is Even Number.................</h4>
+      {num.map((num, index) => {
+        return <li key={index}>{num}</li>;
+      })}
     </div>
   )
 }
