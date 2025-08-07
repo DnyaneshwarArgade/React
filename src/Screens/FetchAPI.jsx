788bc3b5feec035281da76823da4fe35b8a94557
Dnyaneshwar Argade <dnyaneshwarargade8@gmail.com>
2025-08-07 13:11:38 +0530

    fetch data and add functionality search, reset validation search id etc

--- src/Screens/FetchAPI.jsx
+++ src/Screens/FetchAPI.jsx
@@ -1,66 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function FetchAPI() {
 
     const [data, setData] = useState([]);
+    const [filter, setFilter] = useState([]);
     const [search, setSearch] = useState('');
-    const [filterdata, setFilterdata] = useState([]);
 
     const fetchdata = async () => {
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-            const result = await response.json();
-            console.log(result);
-            setData(result);
-            setFilterdata(result);
-        } catch (error) {
-            console.error("Fetch error", error);
-        }
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const result = await response.json();
+        console.log(result);
+        setData(result);
+        setFilter(result);
     };
-
-    const handlereset = () => {
-        setSearch('');
-        setFilterdata(data);
-    };
-
-    const handlefilter = () => {
-        const filtered = search === ''
-            ? data
-            : data.filter(filterall => filterall.id === Number(search));
-        setFilterdata(filtered);
-    }
 
     useEffect(() => {
         fetchdata();
-    }, [])
+    }, []);
+
+    const handleSearch = () => {
+        const filtered = search == ''
+            ? data
+            : data.filter(item => item.id == search);
+        setFilter(filtered);
+    };
+
+    const handleReset = () => {
+        setSearch('');
+        setFilter(data);
+    };
 
     return (
-        <div className='container'>
+        <div>
+            <h4>fetch apit</h4>
+            <input
+                type='number'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder='Enter Id.....!!!'
+                className='form-control w-50'
+            />
 
-            <input type="number" placeholder='Search by Id...' value={search} onChange={(e) => setSearch(e.target.value)} className='form-control mt-4 mb-4 w-25' />
-            <button type='reset' className='btn btn-danger mb-5' onClick={handlereset}>Reset</button>
-            <button type='search' className='btn btn-primary mb-5' onClick={handlefilter}>Search</button>
+            <button type='search' className='btn btn-info mb-5 mt-4' onClick={handleSearch}>Search</button>
+            <button type='reset' className='btn btn-danger mb-5 mt-4' onClick={handleReset}>Reset</button>
 
             <table className='table table-bordered table-striped table-hover table-dark'>
                 <thead>
-                    <tr>
+                    <tr className='text-center'>
                         <th>Id</th>
+                        <th>User Id</th>
                         <th>Title</th>
                         <th>Body</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {filterdata.slice(0, 33).map(user => (
+                    {filter.length > 0 ? (
+                        filter.map(user => (
                         <tr key={user.id}>
-                            <td>{user.id}</td>
+                            <td className='text-center'>{user.id}</td>
+                            <td className='text-center'>{user.userId}</td>
                             <td>{user.title}</td>
                             <td>{user.body}</td>
                         </tr>
-                    ))}
+                    ))
+                ) : (
+                    <tr>
+                        <td colSpan="4" className='text-center'>No data found</td> 
+                    </tr>
+                    )}
                 </tbody>
             </table>
         </div>
     )
 }
-export default FetchAPI;  
+export default FetchAPI;
